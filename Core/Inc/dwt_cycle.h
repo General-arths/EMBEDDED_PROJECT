615e4c09c44b736634005671fc7b036717d1c2ed
General-arths <148326492+General-arths@users.noreply.github.com>
2025-11-01 14:29:29 +0530

    Add DWT cycle counter functions in dwt_cycle.h

--- Core/Inc/dwt_cycle.h
+++ Core/Inc/dwt_cycle.h
@@ -1,1 +1,14 @@
+#pragma once
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void     DWT_CycleCounterInit(void);
+uint32_t DWT_GetCycles(void);
+
+#ifdef __cplusplus
+}
+#endif
 
