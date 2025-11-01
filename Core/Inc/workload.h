8692275f14442abaa1ac6d286a0bc9fbb018fe04
General-arths <148326492+General-arths@users.noreply.github.com>
2025-11-01 14:31:30 +0530

    Add function declarations for workload control

--- Core/Inc/workload.h
+++ Core/Inc/workload.h
@@ -1,1 +1,15 @@
+#pragma once
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void w_ctrl_tick(uintptr_t arg);     // high-priority (TIM1)
+void w_uart_rx_frame(uintptr_t arg); // medium (USART2 IDLE)
+void w_log_flush(uintptr_t arg);     // low (TIM6)
+
+#ifdef __cplusplus
+}
+#endif
 
