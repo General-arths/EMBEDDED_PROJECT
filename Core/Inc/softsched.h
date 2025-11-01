d4646a9e3e8d7d7716022b50667c50855cfa4717
General-arths <148326492+General-arths@users.noreply.github.com>
2025-11-01 14:31:00 +0530

    Add softsched.h for software scheduling functions

--- Core/Inc/softsched.h
+++ Core/Inc/softsched.h
@@ -1,1 +1,20 @@
+#pragma once
+#include <stdbool.h>
+#include <stdint.h>
+#include "ringbuf_spsc.h"
+
+typedef enum { SCHED_HIGH=0, SCHED_MED=1, SCHED_LOW=2 } sched_prio_t;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void SoftSched_Init(void);
+bool SoftSched_PostFromISR(sched_prio_t p, work_fn_t fn, uintptr_t arg);
+void SoftSched_TriggerPendSV(void);
+void SoftSched_RunOnce(void);
+
+#ifdef __cplusplus
+}
+#endif
 
