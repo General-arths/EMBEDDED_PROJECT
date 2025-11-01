b354011b02d688c88a00088fa33ef6b7081d57e0
General-arths <148326492+General-arths@users.noreply.github.com>
2025-11-01 14:29:59 +0530

    Create isr_latency.h for latency control macros
    Add header file for ISR latency management with macros.

--- Core/Inc/isr_latency.h
+++ Core/Inc/isr_latency.h
@@ -1,1 +1,8 @@
+#pragma once
+#include "stm32f4xx_hal.h"
+#include "board_pins.h"
+
+// Ultra-fast set/reset using BSRR (atomic)
+#define LAT_HIGH() (LAT_GPIO_Port->BSRR = LAT_Pin)
+#define LAT_LOW()  (LAT_GPIO_Port->BSRR = ((uint32_t)LAT_Pin << 16u))
 
